cc5719e0d34d7d4b21b2fb65b096ab5367a8a7af
Mental <Alekov99@mail.ru>
2023-02-18 21:50:45 +0500

    Update GameModeBase

--- Source/ShootThemUp/Public/STUCoreTypes.h
+++ Source/ShootThemUp/Public/STUCoreTypes.h
@@ -77,3 +77,12 @@ struct FImpactData
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
     FDecalData DecalData;
 };
+
+USTRUCT(BlueprintType)
+struct FGameData
+{
+    GENERATED_USTRUCT_BODY()
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX", meta = (ClampMin = "1", ClampMax = "100"))
+    int32 PlayersNum = 2;
+};
--- Source/ShootThemUp/Public/STUGameModeBase.h
+++ Source/ShootThemUp/Public/STUGameModeBase.h
@@ -4,12 +4,30 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "STUCoreTypes.h"
 #include "STUGameModeBase.generated.h"
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUGameModeBase : public AGameModeBase
 {
     GENERATED_BODY()
+
 public:
     ASTUGameModeBase();
+
+    virtual void StartPlay() override;
+    UClass* GetDefaultPawnClassForController_Implementation(AController* InController);
+
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    TSubclassOf<class AAIController> AIControllerClass;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    TSubclassOf<class APawn> AIPawnClass;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    FGameData GameData;
+
+private:
+    void SpawnBots();
 };
