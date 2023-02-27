f32f3a6d89646a51d03da1fc96eedc48ed155e5b
Mental <Alekov99@mail.ru>
2023-02-27 10:26:36 +0500

    Fix pickup rotation

--- Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
+++ Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
@@ -57,7 +57,7 @@ void ASTUBasePickup::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
 
-    AddActorLocalRotation(FRotator(0.0f, RotationYaw, 0.0f));
+    AddActorLocalRotation(FRotator(0.0f, RotationYaw, 0.0f) * DeltaTime);
 
     for (const auto OverlappingPawn : OverlappingPawns)
     {
@@ -100,7 +100,7 @@ void ASTUBasePickup::Respawn()
 void ASTUBasePickup::GenerateRotationYaw()
 {
     const float Direction = FMath::RandBool() ? 1.0f : -1.0f;
-    RotationYaw = FMath::RandRange(1.0f, 2.0f) * Direction;
+    RotationYaw = FMath::RandRange(100.0f, 150.0f) * Direction;
 }
 
 void ASTUBasePickup::EnableCollision()
